f3d6c7c94aa49b42bf353df1bf516a511324ce76
tuba tümer <nuraytumer@vivense.com>
2020-06-28 16:48:31 +0300

    Add project files.

---
+++ izin/Data/Enum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace izin.Data
+{
+    public class Enum
+    {
+        public enum RolEnum
+        {
+            [Description("Hepsi")]
+            Hepsi = 1,
+            [Description("Admin")]
+            Admin = 2,
+            [Description("Yonetici")]
+            [Display(Name = "Yönetici")]
+            Yonetici = 3,
+            [Description("Personel")]
+            Personel = 4,
+
+        }
+    }
+}
